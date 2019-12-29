d5cfed3ad2563248d5823eae467a359a99bb8c4c
Zsolt Kozaroczy <kiskoza@gmail.com>
2019-12-29 17:03:27 +0100

    Update test/tc_axlsx.rb
    Co-Authored-By: Stefan Daschek <stefan@daschek.net>

--- test/tc_axlsx.rb
+++ test/tc_axlsx.rb
@@ -24,6 +24,8 @@ class TestAxlsx < Test::Unit::TestCase
 
 
   def test_trust_input_can_be_set_to_true
+    # Class variables like this are not reset between test runs, so we have
+    # to save and restore the original value manually.
     old = Axlsx.trust_input
 
     Axlsx.trust_input = true
