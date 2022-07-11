b9b59ab3e115cde48252fb7f7eec741c20a31ea4
Koza <kiskoza@gmail.com>
2022-07-11 10:16:15 +0200

    Move "capture warning" test helper to its own module

---
+++ test/support/capture_warnings.rb
@@ -0,0 +1,20 @@
+module CaptureWarnings
+  def capture_warnings(&block)
+    # Turn off warnings with setting $VERBOSE to nil
+    original_verbose = $VERBOSE
+    $VERBOSE = nil
+
+    # Redefine warn to redirect warning into our array
+    original_warn = Kernel.instance_method(:warn)
+    warnings = []
+    Kernel.send(:define_method, :warn) { |string| warnings << string }
+    block.call
+
+    # Revert to the original warn method and set back $VERBOSE to previous value
+    Kernel.send(:define_method, :warn, original_warn)
+    $VERBOSE = original_verbose
+
+    # Give back the received warnings
+    warnings
+  end
+end
