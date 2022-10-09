56f0977033d87c29fbcb5a20f0bd61d8fe3fb1ec
Weston Ganger <weston@westonganger.com>
2022-10-09 14:46:36 -0700

    Cleanup serialize calls from axlsx_styler tests

--- test/tc_axlsx_styler.rb
+++ test/tc_axlsx_styler.rb
@@ -3,23 +3,18 @@ require 'tc_helper.rb'
 
 class TestAxlsxStyler < Test::Unit::TestCase
 
-  def setup
-    FileUtils.mkdir_p("tmp/") ### TODO: remove
-  end
-
   def test_to_stream_automatically_performs_apply_styles
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'to_stream_automatically_performs_apply_styles'
     assert_nil wb.styles_applied
     wb.add_worksheet do |sheet|
       sheet.add_row ['A1', 'B1']
       sheet.add_style 'A1:B1', b: true
     end
-    File.open("tmp/#{filename}.xlsx", 'wb') do |f|
-      f.write p.to_stream.read
-    end
+
+    p.to_stream
+
     assert_equal 1, wb.styles.style_index.count
   end
 
@@ -27,22 +22,26 @@ class TestAxlsxStyler < Test::Unit::TestCase
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'without_apply_styles_serialize'
     assert_nil wb.styles_applied
     wb.add_worksheet do |sheet|
       sheet.add_row ['A1', 'B1']
       sheet.add_style 'A1:B1', b: true
     end
-    p.serialize("tmp/#{filename}.xlsx")
+
+    @fname = 'axlsx_test_serialization.xlsx'
+
+    p.serialize(@fname)
+
     assert_equal true, wb.styles_applied
     assert_equal 1, wb.styles.style_index.count
+
+    File.delete(@fname)
   end
 
   def test_merge_styles_1
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'merge_styles_1'
     bold = wb.styles.add_style b: true
 
     wb.add_worksheet do |sheet|
@@ -53,17 +52,16 @@ class TestAxlsxStyler < Test::Unit::TestCase
       sheet.add_style 'B2:D4', b: true
       sheet.add_border 'B2:D4', { style: :thin, color: '000000' }
     end
+
     wb.apply_styles
+
     assert_equal 9, wb.styles.style_index.count
-    p.serialize("tmp/#{filename}.xlsx")
-    assert_equal true, wb.styles_applied
   end
 
   def test_merge_styles_2
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'merge_styles_2'
     bold = wb.styles.add_style b: true
 
     wb.add_worksheet do |sheet|
@@ -72,17 +70,16 @@ class TestAxlsxStyler < Test::Unit::TestCase
       sheet.add_row ['A3', 'B3']
       sheet.add_style 'A1:A2', i: true
     end
+
     wb.apply_styles
+
     assert_equal 3, wb.styles.style_index.count
-    p.serialize("tmp/#{filename}.xlsx")
-    assert_equal true, wb.styles_applied
   end
 
   def test_merge_styles_3
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'merge_styles_3'
     bold = wb.styles.add_style b: true
 
     wb.add_worksheet do |sheet|
@@ -90,17 +87,16 @@ class TestAxlsxStyler < Test::Unit::TestCase
       sheet.add_row ['A2', 'B2']
       sheet.add_style 'B1:B2', bg_color: 'FF0000'
     end
+
     wb.apply_styles
+
     assert_equal 3, wb.styles.style_index.count
-    p.serialize("tmp/#{filename}.xlsx")
-    assert_equal true, wb.styles_applied
   end
 
   def test_table_with_borders
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'borders_test'
     wb.add_worksheet do |sheet|
       sheet.add_row
       sheet.add_row ['', 'Product', 'Category',  'Price']
@@ -120,7 +116,9 @@ class TestAxlsxStyler < Test::Unit::TestCase
       sheet.add_border 'B3:D3', edges: [:bottom], style: :medium
       sheet.add_border 'B3:D3', edges: [:bottom], style: :medium, color: '32f332'
     end
-    p.serialize("tmp/#{filename}.xlsx")
+
+    wb.apply_styles
+
     assert_equal true, wb.styles_applied
     assert_equal 12, wb.styles.style_index.count
     assert_equal 12 + 2, wb.styles.style_index.keys.max
@@ -130,7 +128,6 @@ class TestAxlsxStyler < Test::Unit::TestCase
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'duplicate_borders_test'
     wb.add_worksheet do |sheet|
       sheet.add_row
       sheet.add_row ['', 'B2', 'C2', 'D2']
@@ -140,7 +137,9 @@ class TestAxlsxStyler < Test::Unit::TestCase
       sheet.add_border 'B2:D4'
       sheet.add_border 'B2:D4'
     end
-    p.serialize("tmp/#{filename}.xlsx")
+
+    wb.apply_styles
+
     assert_equal true, wb.styles_applied
     assert_equal 8, wb.styles.style_index.count
     assert_equal 8, wb.styled_cells.count
@@ -150,7 +149,6 @@ class TestAxlsxStyler < Test::Unit::TestCase
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'multiple_style_borders'
     wb.add_worksheet do |sheet|
       sheet.add_row
       sheet.add_row ['', 'B2', 'C2', 'D2']
@@ -159,7 +157,9 @@ class TestAxlsxStyler < Test::Unit::TestCase
       sheet.add_border 'B2:D3', :all
       sheet.add_border 'B2:D2', edges: [:bottom], style: :thick, color: 'ff0000'
     end
-    p.serialize("tmp/#{filename}.xlsx")
+
+    wb.apply_styles
+
     assert_equal true, wb.styles_applied
     assert_equal 6, wb.styles.style_index.count
     assert_equal 6, wb.styled_cells.count
@@ -195,7 +195,6 @@ class TestAxlsxStyler < Test::Unit::TestCase
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'mixed_borders_1'
     wb.add_worksheet do |sheet|
       sheet.add_row
       sheet.add_row ['', '1', '2', '3']
@@ -204,18 +203,17 @@ class TestAxlsxStyler < Test::Unit::TestCase
       sheet.add_style 'B2:D4', border: { style: :thin, color: '000000' }
       sheet.add_border 'C3:D4', style: :medium
     end
+
     wb.apply_styles
+
     assert_equal 9, wb.styled_cells.count
     assert_equal 2, wb.styles.style_index.count
-    p.serialize("tmp/#{filename}.xlsx")
-    assert_equal true, wb.styles_applied
   end
 
   def test_mixed_borders_2
     p = Axlsx::Package.new
     wb = p.workbook
 
-    filename = 'mixed_borders_2'
     wb.add_worksheet do |sheet|
       sheet.add_row
       sheet.add_row ['', '1', '2', '3']
@@ -224,11 +222,11 @@ class TestAxlsxStyler < Test::Unit::TestCase
       sheet.add_border 'B2:D4', style: :medium
       sheet.add_style 'D2:D4', border: { style: :thin, color: '000000' }
     end
+    
     wb.apply_styles
+
     assert_equal 8, wb.styled_cells.count
     assert_equal 6, wb.styles.style_index.count
-    p.serialize("tmp/#{filename}.xlsx")
-    assert_equal true, wb.styles_applied
   end
 
   def test_dxf_cell
@@ -248,8 +246,7 @@ class TestAxlsxStyler < Test::Unit::TestCase
       wb.styles.add_style(bg_color: "0000FF", type: :dxf)
     end
 
-    p.serialize("tmp/test_dxf_cell.xlsx")
-    assert_equal true, wb.styles_applied
+    wb.apply_styles
 
     assert_equal 1, wb.styles.dxfs.count
 
