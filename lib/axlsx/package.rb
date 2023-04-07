fc73023ea783ff7fdeb11ac903baa9142c98cd1c
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-04-07 23:40:27 +0200

    Fix Layout/FirstArrayElementIndentation offense
    ```
    rubocop --only Layout/FirstArrayElementIndentation -a
    ```

--- lib/axlsx/package.rb
+++ lib/axlsx/package.rb
@@ -211,11 +211,11 @@ module Axlsx
     # @private
     def parts
       parts = [
-       {:entry => "xl/#{STYLES_PN}", :doc => workbook.styles, :schema => SML_XSD},
-       {:entry => CORE_PN, :doc => @core, :schema => CORE_XSD},
-       {:entry => APP_PN, :doc => @app, :schema => APP_XSD},
-       {:entry => WORKBOOK_RELS_PN, :doc => workbook.relationships, :schema => RELS_XSD},
-       {:entry => WORKBOOK_PN, :doc => workbook, :schema => SML_XSD}
+        {:entry => "xl/#{STYLES_PN}", :doc => workbook.styles, :schema => SML_XSD},
+        {:entry => CORE_PN, :doc => @core, :schema => CORE_XSD},
+        {:entry => APP_PN, :doc => @app, :schema => APP_XSD},
+        {:entry => WORKBOOK_RELS_PN, :doc => workbook.relationships, :schema => RELS_XSD},
+        {:entry => WORKBOOK_PN, :doc => workbook, :schema => SML_XSD}
       ]
 
       workbook.drawings.each do |drawing|
