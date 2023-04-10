a267015856b6a02cb8adeb1bde7b504c44e683d7
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-04-10 18:11:03 +0200

    Fix Style/BlockDelimiters offenses (TESTS)
    Non-production code
    
    ```
    rubocop --only Style/BlockDelimiters -a
    ```

--- test/benchmark.rb
+++ test/benchmark.rb
@@ -10,7 +10,7 @@ input = (32..126).to_a.pack('U*').chars.to_a
 20.times { row << input.shuffle.join }
 times = 3000
 Benchmark.bmbm(30) do |x|
-  x.report('axlsx_noautowidth') {
+  x.report('axlsx_noautowidth') do
     p = Axlsx::Package.new
     p.workbook do |wb|
       wb.add_worksheet do |sheet|
@@ -21,9 +21,9 @@ Benchmark.bmbm(30) do |x|
     end
     p.use_autowidth = false
     p.serialize("example_noautowidth.xlsx")
-  }
+  end
 
-  x.report('axlsx') {
+  x.report('axlsx') do
     p = Axlsx::Package.new
     p.workbook do |wb|
       wb.add_worksheet do |sheet|
@@ -33,9 +33,9 @@ Benchmark.bmbm(30) do |x|
       end
     end
     p.serialize("example_autowidth.xlsx")
-  }
+  end
 
-  x.report('axlsx_shared') {
+  x.report('axlsx_shared') do
     p = Axlsx::Package.new
     p.workbook do |wb|
       wb.add_worksheet do |sheet|
@@ -46,9 +46,9 @@ Benchmark.bmbm(30) do |x|
     end
     p.use_shared_strings = true
     p.serialize("example_shared.xlsx")
-  }
+  end
 
-  x.report('axlsx_stream') {
+  x.report('axlsx_stream') do
     p = Axlsx::Package.new
     p.workbook do |wb|
       wb.add_worksheet do |sheet|
@@ -59,14 +59,14 @@ Benchmark.bmbm(30) do |x|
     end
     s = p.to_stream
     File.open('example_streamed.xlsx', 'wb') { |f| f.write(s.read) }
-  }
+  end
 
-  x.report('csv') {
+  x.report('csv') do
     CSV.open("example.csv", "wb") do |csv|
       times.times do
         csv << row
       end
     end
-  }
+  end
 end
 File.delete("example.csv", "example_streamed.xlsx", "example_shared.xlsx", "example_autowidth.xlsx", "example_noautowidth.xlsx")
