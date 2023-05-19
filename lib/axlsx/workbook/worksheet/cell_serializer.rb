497457b7a9221944a91d595465599e8451d678e4
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-05-19 12:55:59 +0200

    Fix hash transformation related offenses
    - Style/HashConversion
    - Style/HashTransformKeys
    
    ```
    Comparison (IPS):
          transform_keys:  2890030.0 i/s
               Hash[map]:  1777641.0 i/s - 1.63x  (± 0.00) slower
    
    Comparison (Memory):
          transform_keys:        168 allocated
               Hash[map]:        248 allocated - 1.48x more
    ```

--- lib/axlsx/workbook/worksheet/cell_serializer.rb
+++ lib/axlsx/workbook/worksheet/cell_serializer.rb
@@ -26,7 +26,7 @@ module Axlsx
       def run_xml_string(cell, str = +'')
         if cell.is_text_run?
           valid = RichTextRun::INLINE_STYLES - [:value, :type]
-          data = Hash[Axlsx.instance_values_for(cell).map { |k, v| [k.to_sym, v] }]
+          data = Axlsx.instance_values_for(cell).transform_keys(&:to_sym)
           data = data.select { |key, value| valid.include?(key) && !value.nil? }
           RichText.new(cell.value.to_s, data).to_xml_string(str)
         elsif cell.contains_rich_text?
