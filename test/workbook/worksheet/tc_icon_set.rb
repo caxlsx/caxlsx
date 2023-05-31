86393121fdd30dacf5b54eb64ac30be493405376
Koza <kiskoza@gmail.com>
2023-05-31 10:23:28 +0200

    Add optional interpolation points to icon sets

--- test/workbook/worksheet/tc_icon_set.rb
+++ test/workbook/worksheet/tc_icon_set.rb
@@ -12,12 +12,19 @@ class TestIconSet < Test::Unit::TestCase
     assert(@icon_set.percent)
     refute(@icon_set.reverse)
     assert(@icon_set.showValue)
+    assert_equal([0, 33, 67], @icon_set.interpolationPoints)
   end
 
   def test_icon_set
     assert_raise(ArgumentError) { @icon_set.iconSet = "invalid_value" }
     assert_nothing_raised { @icon_set.iconSet = "5Rating" }
     assert_equal("5Rating", @icon_set.iconSet)
+  end
+
+  def test_interpolation_points
+    assert_raise(ArgumentError) { @icon_set.interpolationPoints = ["invalid_value"] }
+    assert_nothing_raised { @icon_set.interpolationPoints = [0, 60, 80] }
+    assert_equal([0, 60, 80], @icon_set.interpolationPoints)
   end
 
   def test_percent
@@ -43,5 +50,14 @@ class TestIconSet < Test::Unit::TestCase
 
     assert_equal(1, doc.xpath(".//iconSet[@iconSet='3TrafficLights1'][@percent=1][@reverse=0][@showValue=1]").size)
     assert_equal(3, doc.xpath(".//iconSet//cfvo").size)
+  end
+
+  def test_to_xml_string_with_interpolation_points
+    @icon_set.iconSet = '5Arrows'
+    @icon_set.interpolationPoints = [0, 20, 40, 60, 80]
+    doc = Nokogiri::XML.parse(@icon_set.to_xml_string)
+
+    assert_equal(1, doc.xpath(".//iconSet[@iconSet='5Arrows'][@percent=1][@reverse=0][@showValue=1]").size)
+    assert_equal(5, doc.xpath(".//iconSet//cfvo").size)
   end
 end
