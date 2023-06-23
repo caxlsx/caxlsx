e201a69c16b83bb4d956b2b84e383ad4a550dc03
rikweelvoormedia <rikweel@voormedia.com>
2023-06-23 16:49:57 +0200

    Revert "Delete tc_auto_filter.rb"
    This reverts commit 22d0c445dab8b9056532f35febceacd07c43c914.
    
    revert commit.

---
+++ test/workbook/worksheet/auto_filter/tc_auto_filter.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'tc_helper'
+
+class TestAutoFilter < Test::Unit::TestCase
+  def setup
+    ws = Axlsx::Package.new.workbook.add_worksheet
+    ws.add_row ['first', 'second', 'third']
+    3.times { |index| ws.add_row [1 * index, 2 * index, 3 * index] }
+    ws.auto_filter = 'A1:C4'
+    @auto_filter = ws.auto_filter
+    @auto_filter.add_column 0, :filters, filter_items: [1]
+    @auto_filter.sort_state.add_sort_condition 0, order: :desc, custom_list: [0, 2, 1]
+  end
+
+  def test_defined_name
+    assert_equal("'Sheet1'!$A$1:$C$4", @auto_filter.defined_name)
+  end
+
+  def test_to_xml_string
+    doc = Nokogiri::XML(@auto_filter.worksheet.to_xml_string)
+
+    assert_equal('A1:C4', doc.at_xpath('//xmlns:autoFilter')['ref'])
+    assert_equal(2, doc.at_xpath('//xmlns:autoFilter').children.size)
+  end
+
+  def test_columns
+    assert @auto_filter.columns.is_a?(Axlsx::SimpleTypedList)
+    assert_equal @auto_filter.columns.allowed_types, [Axlsx::FilterColumn]
+  end
+
+  def test_add_column
+    @auto_filter.add_column(0, :filters) do |column|
+      assert column.is_a? FilterColumn
+    end
+  end
+
+  def test_apply
+    assert_nil @auto_filter.worksheet.rows.last.hidden
+    assert(@auto_filter.worksheet.rows.last.cells.none? { |cell| cell.value.zero? })
+
+    @auto_filter.apply
+
+    assert @auto_filter.worksheet.rows.last.hidden
+    assert(@auto_filter.worksheet.rows.last.cells.all? { |cell| cell.value.zero? })
+    assert_equal([2, 4, 6], @auto_filter.worksheet.rows[2].map(&:value))
+  end
+end
