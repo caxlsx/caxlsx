43948e8a8580b6da85ba9016958dc3a4abc14fb5
Niklas Bichinger <niklas@bichinger.de>
2023-11-17 11:15:55 +0100

    fix tests

--- lib/axlsx/drawing/pie_chart.rb
+++ lib/axlsx/drawing/pie_chart.rb
@@ -24,10 +24,10 @@ module Axlsx
     # Serializes the object
     # @param [String] str
     # @return [String]
-    def to_xml_string(str = '')
+    def to_xml_string(str = +'')
       super(str) do
         str << '<c:pieChart>'
-        str << ('<c:varyColors val="' << vary_colors.to_s << '"/>')
+        str << '<c:varyColors val="' << vary_colors.to_s << '"/>'
         @series.each { |ser| ser.to_xml_string(str) }
         d_lbls.to_xml_string(str) if @d_lbls
         str << '</c:pieChart>'
